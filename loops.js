3e21c9811b7ecb90cd9907198c80b06dc8a8c060
John Ballon <ballonj1@tcnj.edu>
2017-01-10 21:37:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+
+
+function forLoop(array){
+  for (var i = 0; i < 25; i++){
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(num){
+  while (num > 0){
+    console.log(num)
+    num -= 1
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop(array){
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue());
+  return array
+}
